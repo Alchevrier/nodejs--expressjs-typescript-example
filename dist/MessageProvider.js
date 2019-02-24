9e5ec7db6f248535830aef0b22f0f0de09cf93df
Alex Chevrier <chevrier.alex@gmail.com>
2019-02-24 18:27:55 +0000

    Removing output code

--- dist/MessageProvider.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MessageProvider {
-    provide() {
-        return "Hello World!";
-    }
-}
-exports.MessageProvider = MessageProvider;
-//# sourceMappingURL=MessageProvider.js.map
